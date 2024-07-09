b65402cefe753a67167e53d2a82bd70eb808974c
Alan Sagot <121070452+AlanSagot@users.noreply.github.com>
2024-07-09 15:23:31 -0600

    Arreglo de vistas
    Se aplicaron algunas correcciones en las vistas

--- DAL/Inventario.cs
+++ DAL/Inventario.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL
 {
@@ -14,37 +11,60 @@ namespace DAL
     {
         [Key]
         public int ProductoId { get; set; }
+
         [Required]
         [MaxLength(150)]
         public string Codigo { get; set; }
-        public byte[]? FotoProducto{ get; set; }
-        [DisplayName("Nombre del Producto")]
+
+        public byte[]? FotoProducto { get; set; }
+
+        [DisplayName("Producto")]
         public string NombreProducto { get; set; }
+
         [MaxLength(500)]
         public string Categoria { get; set; }
-        [Required]
-        [DisplayName("Descripcion del Producto")]
 
+        [Required]
+        [DisplayName("Descripción")]
         public string DescripcionProducto { get; set; }
+
         [Required]
         public string Marca { get; set; }
+
         [Required]
+        [DisplayName("Precio Venta")]
         public int PrecioXVenta { get; set; }
+
         [Required]
+        [DisplayName("Precio Costo")]
         public int PrecioXCosto { get; set; }
+
         [Required]
         public int Stock { get; set; }
+
+        private DateTime _fechaAgregado;
         [DisplayName("Fecha Agregado")]
-        public DateTime FechaAgregado { get; set; }
+        public DateTime FechaAgregado
+        {
+            get => _fechaAgregado.Date;
+            set => _fechaAgregado = value.Date;
+        }
+
+        private DateTime _fechaExpiracion;
         [DisplayName("Fecha Expiracion")]
-        public DateTime FechaExpiracion { get; set; }
+        public DateTime FechaExpiracion
+        {
+            get => _fechaExpiracion.Date;
+            set => _fechaExpiracion = value.Date;
+        }
+
         [DisplayName("Sucursal")]
         [ForeignKey("Sucursal")]
         public int SucursalId { get; set; }
         public Sucursal Sucursal { get; set; }
+
         public ICollection<Catalogos> Catalogos { get; set; } = new List<Catalogos>();
         public ICollection<Envio> Envios { get; set; } = new List<Envio>();
         public ICollection<Factura> Facturas { get; set; } = new List<Factura>();
     }
-
 }
