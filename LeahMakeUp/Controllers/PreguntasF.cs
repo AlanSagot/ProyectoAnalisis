463d89a4bc990a7eb3d75d230f9039391df95045
Alan Sagot <121070452+AlanSagot@users.noreply.github.com>
2024-08-16 21:40:31 -0600

    Cambios en los diseños y FAQ

---
+++ LeahMakeUp/Controllers/PreguntasF.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeahMakeUp.Controllers
+{
+    public class PreguntasFController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
