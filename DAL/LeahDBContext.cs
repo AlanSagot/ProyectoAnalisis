86ab7715dc5432c0afe3a2b2f6b6b528f2474b71
Nazareth Murillo <nazznmv241020@gmail.com>
2024-08-19 02:50:47 -0600

    :alien:

--- DAL/LeahDBContext.cs
+++ DAL/LeahDBContext.cs
@@ -20,11 +20,10 @@ namespace DAL
         public DbSet<Empleado> Empleados { get; set; }
         public DbSet<Estado> Estados { get; set; }
         public DbSet<Marca> Marcas { get; set; }
-
         public DbSet<Categoria> Categorias { get; set; }
-
         public DbSet<Envio> Envios { get; set; }
         public DbSet<Factura> Facturas { get; set; }
+        public DbSet<Incapacidades> Incapacidades { get; set; }
         public DbSet<Inventario> Inventarios { get; set; }
         public DbSet<Pago> Pagos { get; set; }
         public DbSet<Planillas> Planillas { get; set; }
